d15b20da83b0848331670c61c2a20c0a859b39f2
masatowaseda <katagirisyuukatu@gmail.com>
2021-08-20 14:00:03 +0000

    basic signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "help"=>"static_pages#help"
   get "about"=>"static_pages#about"
   get "contact"=>"static_pages#contact"
+  resources:users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  # 以下のコードを追加してみましょう
 end
